9180d5ce233c7967caf42d699544eff445ae1907
Kousay Jebir <kousayjebir@gmail.com>
2026-09-24 15:53:36 +0100

    changed gitignore

--- test/index.js
+++ test/index.js
@@ -20,7 +20,68 @@ const naiveStrategy = () => {
     }
 }
 
-backTest('../backtest-data', naiveStrategy, {
+const naiveStrategy4 = () => {
+    return (marketEntry, { buy, up, down, end, differenceInSeconds, firstMarketEntry, lastMarketEntry }) => {
+        if (up.best_ask <= 0.13 && up.best_ask >= 0.12) {
+            buy('up', 1)
+            if (up.best_ask >= 0.95) {
+                //buy('down', 1)
+            }
+            end()
+        }
+        else if (down.best_ask <= 0.13 && down.best_ask >= 0.12) {
+            buy('down', 1)
+            if (down.best_ask >= 0.95) {
+                //buy('up', 1)
+            }
+            end()
+        }
+    }
+}
+
+
+
+const naiveStrategy2 = () => {
+    return (marketEntry, { buy, up, down, end, differenceInSeconds, firstMarketEntry, lastMarketEntry }) => {
+        if (up.best_ask >= 0.9 && up.best_ask <= 0.91 && differenceInSeconds(marketEntry.timestamp, lastMarketEntry.timestamp) >= 40 && differenceInSeconds(marketEntry.timestamp, lastMarketEntry.timestamp) <= 45) {
+            buy('down', 1)
+            if (up.best_ask >= 0.95) {
+                //buy('up', 5)
+            }
+            end()
+        }
+        else if (down.best_ask >= 0.9 && down.best_ask <= 0.91 && differenceInSeconds(marketEntry.timestamp, lastMarketEntry.timestamp) >= 40 && differenceInSeconds(marketEntry.timestamp, lastMarketEntry.timestamp) <= 45) {
+            buy('up', 1)
+            if (down.best_ask >= 0.95) {
+                //buy('up', 5)
+            }
+            end()
+        }
+    }
+}
+
+
+const naiveStrategy3 = () => {
+    return (marketEntry, { buy, up, down, end, differenceInSeconds, firstMarketEntry, lastMarketEntry }) => {
+        if (up.best_ask >= 0.9
+        ) {
+            buy('up', 1)
+            if (up.best_ask >= 0.95) {
+
+            }
+            end()
+        }
+        else if (down.best_ask >= 0.9) {
+            buy('down', 1)
+            if (down.best_ask >= 0.95) {
+
+            }
+            end()
+        }
+    }
+}
+
+backTest('../backtest-data', naiveStrategy4, {
     balance: 10
 }).then((result) => {
     fs.writeFileSync(
